ccd5e17bf2f6443a05b693144f8d562b151e781e
Austin Mills <austin.mills@oracle.com>
2013-12-21 23:11:42 -0600

    Initial checkin post-rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChristmasElf::Application.config.secret_key_base = 'd98c01a979c96e70656c392d570ef6c45e2bdeffe9e5ce625a7266c63aac2cbfe5cb4bd23357418fc29fb187aad5f13e64dc7f01df0ae4b64069827ade0af04d'
